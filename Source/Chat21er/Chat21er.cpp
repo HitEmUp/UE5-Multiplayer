6d3b425e526c168f9731d94b8b66f63a7c3837f0
HitEmUp <samanrazavi@web.de>
2024-02-29 18:54:59 +0100

    Alles neu

--- Source/Chat21er/Chat21er.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Chat21er.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chat21er, "Chat21er" );
